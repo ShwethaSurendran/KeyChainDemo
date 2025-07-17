a98ce3af8295fb00d830d942b6955a62a6a589fc
Shwetha <shwetha11393@gmail.com>
2025-07-17 14:24:26 +1000

    -Added sharedKeychain item in entitlement, Updated UI

--- KeyChainDemo/ContentVM.swift
+++ KeyChainDemo/ContentVM.swift
@@ -11,57 +11,61 @@ class ContentViewVM: ObservableObject {
     @Published var keychainStr:String = ""
     @Published var valueFromKeychain:String = ""
     @Published var updateKeyChainValue:String = ""
+    @Published var alertMessage:String = ""
     let keychain: KeyChainSwift = KeyChainSwift()
     
     @MainActor
-    func addToKeychain() {
-        Task {
-            await keychain.setAccessGroup("N4BS4WAD88.com.ns.NewApp.KeyChainDemo")
+    func addToKeychain() async {
+        //Keychain will get this value from entitlements if not set manually
+//            await keychain.setAccessGroup("N4BS4WAD88.com.ns.NewApp.KeyChainDemo")
+        
             do {
                 print(try await keychain.set(keychainStr, forKey: "com.test1",withAccessType: kSecAttrAccessibleAfterFirstUnlock))
                 keychainStr = ""
+                alertMessage = "Successfully added"
             } catch {
+                alertMessage = error.localizedDescription
                 print(error)
             }
-        }
     }
     
     @MainActor
-    func readFromKeychain() {
-        Task {
+    func readFromKeychain() async {
             do {
                 let value = try await keychain.getValueFor("com.test1",withAccessType: kSecAttrAccessibleAfterFirstUnlock)
                 print(value)
                 valueFromKeychain = value
+                alertMessage = "Successfully read"
             } catch {
+                alertMessage = error.localizedDescription
                 print(error)
             }
-        }
     }
     
     @MainActor
-    func updateKeychain() {
-        Task {
+    func updateKeychain() async {
             do {
                 let value = try await keychain.update(updateKeyChainValue,
                                                       forKey: "com.test1",
                                                       withAccessType: kSecAttrAccessibleAfterFirstUnlock)
                 print(value)
+                alertMessage = "Successfully updated"
             } catch {
+                alertMessage = error.localizedDescription
                 print(error)
             }
-        }
     }
     
-    func delete() {
-        Task {
+    @MainActor
+    func delete() async {
             do {
                 let value = try await keychain.delete(forKey: "com.test1",
                                                       withAccessType: kSecAttrAccessibleAfterFirstUnlock)
                 print(value)
+                alertMessage = "Successfully deleted"
             } catch {
+                alertMessage = error.localizedDescription
                 print(error)
             }
-        }
     }
 }
--- KeyChainDemo/ContentView.swift
+++ KeyChainDemo/ContentView.swift
@@ -7,40 +7,92 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @StateObject var viewModel = ContentViewVM()
+    @State var showAlert: Bool = false
+    
     var body: some View {
-        VStack {
-            TextField("Add to Key chain", text: $viewModel.keychainStr)
-                .frame(height: 50)
-                .border(.gray)
-            Button("Add To Key Chain") {
-                viewModel.addToKeychain()
+        VStack(spacing: 50) {
+            HStack {
+                TextField("Type here", text: $viewModel.keychainStr)
+                    .modifier(TextFieldModifier())
+                CustomButton(label: "Add To Keychain") {
+                    Task {
+                        await viewModel.addToKeychain()
+                        showAlert = true
+                    }
+                }
             }
-            .padding(.top)
+
+            HStack() {
+                CustomButton(label: "Read from Keychain") {
+                    Task {
+                        await viewModel.readFromKeychain()
+                        showAlert = true
+                    }
+                }
+                Text(viewModel.valueFromKeychain)
+                    .padding()
+                    .font(.system(size: 15, weight: .bold))
+            }
+
+            HStack {
+                TextField("Type here", text: $viewModel.updateKeyChainValue)
+                    .modifier(TextFieldModifier())
+                CustomButton(label: "Update Keychain") {
+                    Task {
+                        await viewModel.updateKeychain()
+                        showAlert = true
+                    }
+                }
+            }
             
-            Text(viewModel.valueFromKeychain)
-                .padding()
-            Button("Read from Key Chain") {
-                viewModel.readFromKeychain()
+            CustomButton(label: "Delete") {
+                Task {
+                    await viewModel.delete()
+                    showAlert = true
+                }
             }
-            .padding(.top)
-            
-            TextField("Update Key chain", text: $viewModel.updateKeyChainValue)
-                .frame(height: 50)
-                .border(.gray)
-            Button("Update Key Chain") {
-                viewModel.updateKeychain()
-            }
-            .padding(.top)
-            
-            Button("Delete") {
-                viewModel.delete()
-            }
-            .padding(.top)
             
         }
         .padding()
+        .alert(viewModel.alertMessage, isPresented: $showAlert) {
+            Button("OK", role: .cancel) {
+                viewModel.alertMessage = ""
+            }
+        }
     }
+}
+
+struct TextFieldModifier: ViewModifier
+{
+    func body(content: Content) -> some View
+    {
+        content
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(style: StrokeStyle(lineWidth: 1))
+            )
+    }
+}
+
+struct CustomButton: View {
+    var label: String
+    var action: () -> Void
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            Text(label)
+                .foregroundColor(.black)
+                .padding(10)
+                .background(.gray)
+                .cornerRadius(10)
+        }
+    }
+    
 }
 
 #Preview {
